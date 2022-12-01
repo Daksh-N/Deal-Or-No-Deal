7e46da1404fb1e8aa685e75a4a297c8a66f373e5
Daksh <dnakra@bsd220.org>
2022-12-01 14:25:44 -0600

    Initial Commit

---
+++ Deal Or No Deal/Deal_Or_No_DealApp.swift
@@ -0,0 +1,17 @@
+//
+//  Deal_Or_No_DealApp.swift
+//  Deal Or No Deal
+//
+//  Created by Daksh Nakra on 12/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Deal_Or_No_DealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
